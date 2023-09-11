27923d5a7a2031defaa46cc4fd64bda8c40523fa
prashanthkeetha <prashanthkeetha358@gmail.com>
2023-09-11 13:30:40 +0530

    adding changes in arrowfunctions

--- functions.js
+++ functions.js
@@ -27,18 +27,41 @@ console.log("Result:", number)//output:Result: 22*/
 we get the input as with multiple digits we get output all the values at once. 
 eg: 
 -->TYPE1*/
-let num1 = (200,300,400,500);
-function caluclatingValues(...num1){
-    return num1
-}
-console.log(num1);
+// let num1 = (200,300,400,500);
+// function caluclatingValues(...num1){
+//     return num1
+// }
+// console.log(num1);
 /*output:500*/
 /*TYPE2:*/
-function calculateValues(...num2){
-    return num2
-} 
-console.log(calculateValues(600,800,1000,1500));
+// function calculateValues(...num2){
+//     return num2
+// } 
+// console.log(calculateValues(600,800,1000,1500));
 /*Output:[ 600, 800, 1000, 1500 ]*/
 //---->Nested function: Functions inside the function is nested Function.
 /*ARROW FUNCTIONS AND THIS KEYWORD*/
+// const cam = {
+//     brand: "Canon",
+//     price: 62000,
+//     welcomeMessage: function() {
+//         console.log(`${this.brand}, best picture quality`);
+//     }
+// };
 
+// Calling the welcomeMessage function
+// cam.welcomeMessage();
+/*--->IN THE ARROW FUNCTION WE HAVE TWO TYPES THE CODE WE WRITE
+ TYPE1:
+ HERE WE NEED TO WRITE THE RETURN TO EXECUTE THE CODE*/
+// const sub=(part1,part2)=>{
+//     return(part1-part2)
+// } 
+// console.log(sub(10,4));
+//IF THE CODE WRITE IN THE CURL BRACKETS{}, MOST NEED TO WRITE THE RETURN VALUE
+//TYPE2:THIS IS IMPLECIT IS WHERE WE CAN WRITE THE CODE BY USING THE 
+const add =(num1,num2)=>(num1+num2)
+console.log(add(4,8))
+//-----> In this if we want to write the object we can use curl brackets
+const addTwo= (user,course)=>({user:"prashanth",course:"js"})
+console.log(addTwo());
